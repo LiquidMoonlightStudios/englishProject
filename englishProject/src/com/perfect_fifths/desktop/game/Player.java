8c9ea60e99c9a65eb334a038153b03f032da3f83
Spike35031 <ben.jg.turner@gmail.com>
2015-01-11 13:19:39 -0800

    Make Player Serializable

--- englishProject/src/com/perfect_fifths/desktop/game/Player.java
+++ englishProject/src/com/perfect_fifths/desktop/game/Player.java
@@ -1,12 +1,16 @@
 package com.perfect_fifths.desktop.game;
 
 import java.awt.Graphics;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 import com.perfect_fifths.asset_classes.Animation;
 
-public class Player {
-	int x;
-	int y;
+public class Player implements java.io.Serializable {
+	private static final long serialVersionUID = 8729574840294597942L;
+	public static final int STATIC = 0, UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4;
+	transient int x;
+	transient int y;
 	Animation[] animations;
 	int currentAnimation = 0;
 	
@@ -28,5 +32,10 @@ public class Player {
 
 	public void paint(Graphics g) {
 		animations[currentAnimation].paint(g, x, y);
+	}
+	
+	private void readObject(ObjectInputStream in) {
+		x = 0;
+		y = 0;
 	}
 }
